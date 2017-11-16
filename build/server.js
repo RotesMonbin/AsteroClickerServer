d78207a36ceee6eaba5697469c65c39e2970292b
Robin <robinboitemail@gmail.com>
2017-11-16 15:48:16 +0100

    added : asteroid seed

--- build/server.js
+++ build/server.js
@@ -50,6 +50,9 @@ io.on("connection", (socket) => {
     socket.on('buyOre', (message) => {
         buyOre(message);
     });
+    socket.on('searchAster', (message) => {
+        searchAster(message);
+    });
 });
 function incrementOre(data) {
     defaultDatabase.ref("users/" + data.user).once('value').then((user) => {
@@ -105,6 +108,30 @@ function buyOre(data) {
             }
         });
     });
+}
+function searchAster(data) {
+    const asteNum = getNewAsteroidType();
+    const seed = generateRandomNumber(4) + generateRandomNumber(4);
+    defaultDatabase.ref("users/" + data.user + "/asteroid/numAsteroid").set(asteNum);
+    defaultDatabase.ref("users/" + data.user + "/asteroid/seed").set(seed);
+}
+function getNewAsteroidType() {
+    return Math.floor(Math.random() * asteroidTypes.length);
+}
+function generateRandomNumber(range) {
+    let seed = "";
+    let nums = [];
+    for (let i = 0; i < range; i++) {
+        nums[i] = i;
+    }
+    let i = nums.length;
+    let j = 0;
+    while (i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        seed = seed + "" + nums[j];
+        nums.splice(j, 1);
+    }
+    return seed;
 }
 function getUpgradeFromString(name) {
     switch (name) {
--- src/server.ts
+++ src/server.ts
@@ -61,6 +61,10 @@ io.on("connection", (socket: SocketIO.Socket) => {
         buyOre(message);
     });
 
+    socket.on('searchAster', (message) => {
+        searchAster(message);
+    });
+
 })
 
 /*
@@ -153,6 +157,47 @@ function buyOre(data) {
     });
 }
 
+/*
+data = {
+    user : userId
+}
+*/
+
+function searchAster(data) {
+    //defaultDatabase.ref("users/" + data.user).once('value').then((user) => {
+    const asteNum = getNewAsteroidType();
+    const seed = generateRandomNumber(4)+generateRandomNumber(4);
+    defaultDatabase.ref("users/" + data.user + "/asteroid/numAsteroid").set(asteNum);
+    defaultDatabase.ref("users/" + data.user + "/asteroid/seed").set(seed);
+
+    // });
+}
+
+function getNewAsteroidType() {
+    return Math.floor(Math.random() * asteroidTypes.length);
+}
+
+function generateRandomNumber(range: number) {
+
+    let seed = "";
+    let nums = [];
+
+    for (let i = 0; i < range; i++) {
+        nums[i] = i;
+    }
+
+    let i = nums.length;
+    let j = 0;
+
+    while (i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        seed = seed + "" + nums[j];
+        nums.splice(j, 1);
+    }
+
+    return seed;
+}
+
 
 function getUpgradeFromString(name) {
     switch (name) {
@@ -194,6 +239,6 @@ function loadAsteroidTypes() {
     });
 }
 
-function toFixed2(number){
+function toFixed2(number) {
     return parseFloat(number.toFixed(2));
 }
