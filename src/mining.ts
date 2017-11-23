0f8f6aa7be18922f149d2d891c9a09006398b763
Robin <robinboitemail@gmail.com>
2017-11-23 16:05:53 +0100

    merge

--- src/mining.ts
+++ src/mining.ts
@@ -13,13 +13,10 @@ data = {
 */
 export function incrementOre(data) {
     defaultDatabase.ref("users/" + data.user).once('value').then((user) => {
-<<<<<<< HEAD
-        const maxMinerate = (mineRateUpgrade[user.val().mineRateLvl].maxRate *
+
+        const maxMinerate = (mineRateUpgrade[user.val().upgrade.mineRateLvl].maxRate *
             oreInfo[user.val().asteroid.ore].miningSpeed * (user.val().asteroid.purity / 100)) + 0.1; // +0.1 to avoid false comparison
-=======
-        const maxMinerate = mineRateUpgrade[user.val().upgrade.mineRateLvl].maxRate *
-            oreInfo[user.val().asteroid.ore].miningSpeed;   
->>>>>>> 676c2bcff5d6c804451f1b944693ddac9cfe02cc
+
         if (data.amount <= maxMinerate) {
             const currentAmount = user.val().ore[data.ore];
             const maxAmount = storageUpgrade[user.val().upgrade.storageLvl].capacity;
