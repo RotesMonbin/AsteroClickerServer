6db7f6bc333560bb8fded96593360406678e0040
Niko <niikosaul@gmail.com>
2018-01-03 15:20:59 +0100

    QuestFull random

--- src/upgrade.ts
+++ src/upgrade.ts
@@ -1,5 +1,5 @@
 import { defaultDatabase } from "./environment"
-import { getUpgradeFromString } from "./resources";
+import { getUpgradeFromString, oreInfo } from "./resources";
 import { checkQuest } from "./quest";
 import { calculScore } from "./ranking";
 import { toFixed2 } from "./utils";
@@ -10,7 +10,7 @@ import { toFixed2 } from "./utils";
  * 
  */
 
-export function upgradeShip(data) {
+export function upgradeShipCredit(data) {
     defaultDatabase.ref("users/" + data.user).once('value').then((user) => {
         if (user.val().upgrade[data.upgrade].start == 0) {
             const currentLvl = user.val().upgrade[data.upgrade].lvl;
@@ -25,6 +25,51 @@ export function upgradeShip(data) {
     });
 }
 
+/**
+ * 
+ * @param message [user] : userId, [upgrade]: upgradeName
+ * 
+ */
+export function upgradeShipOre(data) {
+    defaultDatabase.ref("users/" + data.user).once('value').then((user) => {
+        if (user.val().upgrade[data.upgrade].start == 0) {
+            const currentLvl = user.val().upgrade[data.upgrade].lvl;
+            const cost = costOreUpgrade(getUpgradeFromString(data.upgrade)[currentLvl + 1].cost * 0.9, data.upgrade);
+            const nameOre_0 = valuesOreForUpgrade(data.upgrade)[0];
+            const nameOre_1 = valuesOreForUpgrade(data.upgrade)[1];
+            if (user.val().ore[nameOre_0] >= cost[0] && user.val().ore[nameOre_1] >= cost[1]) {
+                defaultDatabase.ref("users/" + data.user + "/ore/" + nameOre_0).set(toFixed2(user.val().ore[nameOre_0] - cost[0]));
+                defaultDatabase.ref("users/" + data.user + "/ore/" + nameOre_1).set(toFixed2(user.val().ore[nameOre_1] - cost[1]));
+                defaultDatabase.ref("users/" + data.user + "/upgrade/" + data.upgrade + "/start").set(Date.now());
+                checkQuest('upgrade' + data.upgrade, 1, user.val(), data.user);
+                calculScore(getUpgradeFromString(data.upgrade)[currentLvl + 1].cost, user.val(), data.user);
+            }
+        }
+    });
+}
+
+function costOreUpgrade(costCredit: number, nameUpgrade: string) {
+    return [ costOreForCredit(valuesOreForUpgrade(nameUpgrade)[0], costCredit/2) , costOreForCredit(valuesOreForUpgrade(nameUpgrade)[1], costCredit/2) ];
+}
+
+function costOreForCredit(nameOre: string, costCredit: number) {
+    return costCredit / oreInfo[nameOre].meanValue;
+}
+
+function valuesOreForUpgrade(nameUpgrade: string) {
+    switch (nameUpgrade) {
+        case 'engine':
+            return ['titanium', 'hyperium'];
+        case 'storage':
+            return ['carbon', 'titanium'];
+        case 'research':
+            return ['carbon', 'iron'];
+        case 'mineRate':
+            return ['titanium', 'iron'];
+        default:
+         return 0;
+    }
+}
 /**
  * 
  * @param message [user] : userId, [upgrade]: upgradeName
