230e9416ca3a371299def99a61b29f311914b0b0
Niko <niikosaul@gmail.com>
2018-01-25 16:26:00 +0100

    fix rank

--- src/ranking.ts
+++ src/ranking.ts
@@ -3,7 +3,7 @@ import { toFixed2, sort_by } from "./utils";
 
 // Ranking managed
 export function calculScore(amount: number, user, userID: number) {
-    defaultDatabase.ref("users/" + userID + "/score").set(amount + user.upgrade.score);
+    defaultDatabase.ref("users/" + userID + "/score").set(amount + user.score);
 }
 
 // Calcul the ranking with the score
